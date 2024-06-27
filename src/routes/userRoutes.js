80a702213d119b8aad621fa9c9d189d60512a2ff
Lucas01v <lucasvivanco01@outlook.com>
2024-06-27 17:14:02 -0300

    Modificación para listar mascotas en adopción

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,21 +2,22 @@ const express = require('express');
 const { registerUser, getUserPets} = require('../Controller/userController');
 const { createAppointment, getAppointments, getAppointmentsByDate, cancelAppointment} = require('../Controller/appointmentController');
 const authMiddleware = require('../middleware/authMiddleware'); // middleware de autenticación
-const { adoptPet, getAdoptionsByUserId } = require('../Controller/adoptionController');
+const { adoptPet, listAdoptablePets, getAdoptionsByUserId, getAllAdoptions, } = require('../Controller/adoptionController');
 
 const userRouter = express.Router();
 
 //MASCOTAS
-userRouter.get('/:userId/pets', authMiddleware, getUserPets);
+userRouter.get('/:userId/pets', getUserPets); //Listar mascotas de un usuario 
 
 //TURNOS (modificar ruta el nombre y probar)
-userRouter.post('/:userId/new_appointment', authMiddleware, createAppointment);
-userRouter.get('/:userId/get_appointments', authMiddleware, getAppointments);
-userRouter.get('/:userId/appointments_date', authMiddleware, getAppointmentsByDate);
-userRouter.delete('/:userId/:appointmentId', authMiddleware, cancelAppointment);
+userRouter.post('/:userId/new_appointment', authMiddleware, createAppointment); //Crear turno de usuario
+userRouter.get('/:userId/get_appointments', authMiddleware, getAppointments);   //Listar todos los turnnos de usuario
+userRouter.get('/:userId/appointments_date', authMiddleware, getAppointmentsByDate); //Listar turnos de usuario por fecha
+userRouter.delete('/:userId/:appointmentId', authMiddleware, cancelAppointment); //Cancelar Turno
 
 //ADOPCIÓN
-userRouter.post('/adoption/adopt', authMiddleware, adoptPet);
-userRouter.get('/adoption', authMiddleware, getAdoptionsByUserId);
+userRouter.get('/:userId/adoption', getAdoptionsByUserId) //Listar adopciones por ID de usuario
+userRouter.get('/adoption/adopt_pets', authMiddleware, listAdoptablePets); //Listar mascotas en adopción (sin dueño asignado)
+userRouter.post('/adoption/adopt', authMiddleware, adoptPet); //Solicitar adopción
 
 module.exports = userRouter;
